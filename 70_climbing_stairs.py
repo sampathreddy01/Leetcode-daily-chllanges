90b5b9bb8967b9edfeb01b81b93118795adc57ad
Sampath Reddy Ette <39610352+sampathreddy01@users.noreply.github.com>
2024-01-18 20:00:19 -0700

    Jan 18th

---
+++ 70_climbing_stairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        dp=[0]*n
+        if(n>=1):
+            dp[0]=1
+        if(n>=2):
+            dp[1]=2
+        for i in range(2,n):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n-1]
