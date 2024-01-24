d3ebb11b86a03842c72771d72a1b62dd908ea622
Sampath Reddy Ette <39610352+sampathreddy01@users.noreply.github.com>
2024-01-24 12:09:51 -0700

    Jan 24th

---
+++ 1457_pseudo_palindromic_paths.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def pseudoPalindromicPaths (self, root):
+        def traverse(node, pairs):
+            if not node:
+                return 0
+            
+            if node.val in pairs:
+                pairs.remove(node.val)
+            else:
+                pairs.add(node.val)
+            
+            if not node.left and not node.right:
+                return 1 if len(pairs) <= 1 else 0
+            
+            left = traverse(node.left, set(pairs))
+            right = traverse(node.right, set(pairs))
+            
+            return left + right
+        return traverse(root, set())
